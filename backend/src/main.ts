e0b51e93b1a40dddcd33b4293c7eb8899055a4b3
canvassed <tydickson255@gmail.com>
2026-06-02 18:51:37 -0400

    fly

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import WebSocket from 'ws';
+
+
+if (!globalThis.WebSocket) {
+  (globalThis as any).WebSocket = WebSocket;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await app.listen(process.env.PORT ?? 3000);
+
+
 
   
 }
