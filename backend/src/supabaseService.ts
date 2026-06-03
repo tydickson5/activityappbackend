495fcc6ef1820e4dbf21d56b70bb8513a0c6b751
canvassed <tydickson255@gmail.com>
2026-06-02 20:00:18 -0400

    fix

--- backend/src/supabaseService.ts
+++ backend/src/supabaseService.ts
@@ -7,10 +7,6 @@ export class SupabaseService {
   readonly client = createClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    {
-      realtime: {
-        transport: ws as any,
-      },
-    }
+
   );
 }
